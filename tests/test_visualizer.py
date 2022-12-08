3219b3590a0cd634f52cb3f1c08d1fd1f360d83f
Murillo Rojas, Luis <luis.murillo.rojas@intel.com>
2022-12-08 07:12:00 -0800

    Adapt visualizer tests to PerturbedImageVisualizer changes

--- tests/test_visualizer.py
+++ tests/test_visualizer.py
@@ -7,17 +7,10 @@
 
 from unittest.mock import Mock
 
-import pytest
-import torch
 from PIL import Image, ImageChops
 from torchvision.transforms import ToPILImage
 
 from mart.attack.callbacks import PerturbedImageVisualizer
-
-
-def perturb(input, target, perturbation, **kwargs):
-    result = [sample + perturbation for sample in input]
-    return result
 
 
 def test_visualizer_run_end(input_data, target_data, perturbation, tmp_path):
@@ -25,18 +18,20 @@ def test_visualizer_run_end(input_data, target_data, perturbation, tmp_path):
     input_list = [input_data]
     target_list = [target_data]
 
+    # simulate an addition perturbation
+    def perturb(input, target):
+        result = [sample + perturbation for sample in input]
+        return result
+
     model = Mock()
-    adversary = Mock()
-    adversary.perturber.return_value = perturbation
-    adversary.threat_model.side_effect = perturb
+    adversary = Mock(side_effect=perturb)
 
     visualizer = PerturbedImageVisualizer(folder)
     visualizer.on_run_end(adversary, input_list, target_list, model)
 
+    # verify that the visualizer created the JPG file
     expected_output_path = folder / target_data["file_name"]
     assert expected_output_path.exists()
-    adversary.perturber.assert_called_once_with(input_list, target_list)
-    adversary.threat_model.assert_called_once_with(input_list, target_list, perturbation)
 
     # verify image file content
     perturbed_img = input_data + perturbation
