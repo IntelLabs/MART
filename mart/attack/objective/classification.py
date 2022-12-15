b79bee28def62b1e0e2763baa4227b89ac44e88c
Murillo Rojas, Luis <luis.murillo.rojas@intel.com>
2022-12-15 12:06:09 -0800

    Add type annotation to Objectives

--- mart/attack/objective/classification.py
+++ mart/attack/objective/classification.py
@@ -15,7 +15,7 @@ __all__ = ["Mispredict", "RandomTarget"]
 
 
 class Mispredict(Objective):
-    def __call__(self, input: torch.Tensor, target: torch.Tensor):
+    def __call__(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         # FIXME: I don't like this argmax call. It feels like this should receive input tensor of
         #        the same shape as target?
         mispredictions = input.argmax(dim=-1) != target
@@ -29,7 +29,7 @@ class RandomTarget(Objective):
         self.nb_classes = nb_classes
         self.gain_fn = gain_fn
 
-    def __call__(self, logits: torch.Tensor, target: torch.Tensor):
+    def __call__(self, logits: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         # FIXME: It may be better if we make sure that the pseudo target is different from target.
         pseudo_target = torch.randint_like(target, low=0, high=self.nb_classes)
         return self.gain_fn(logits, pseudo_target)
