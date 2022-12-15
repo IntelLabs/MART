0cd5f0c3360ac10cd801bd4d196eac37ee93d51c
Murillo Rojas, Luis <luis.murillo.rojas@intel.com>
2022-12-15 12:19:38 -0800

    Add type annotation to Callback

--- mart/attack/callbacks/base.py
+++ mart/attack/callbacks/base.py
@@ -6,6 +6,11 @@
 #
 
 import abc
+from typing import Any, Dict, Union
+
+import torch
+
+from mart.nn import SequentialDict
 
 __all__ = ["Callback"]
 
@@ -13,20 +18,62 @@ __all__ = ["Callback"]
 class Callback(abc.ABC):
     """Abstract base class of callbacks."""
 
-    def on_run_start(self, adversary, input, target, model, **kwargs):
+    def on_run_start(
+        self,
+        adversary: torch.nn.Module,
+        input: Union[torch.Tensor, tuple],
+        target: Union[torch.Tensor, Dict[str, Any], tuple],
+        model: SequentialDict,
+        **kwargs
+    ):
         pass
 
-    def on_examine_start(self, adversary, input, target, model, **kwargs):
+    def on_examine_start(
+        self,
+        adversary: torch.nn.Module,
+        input: Union[torch.Tensor, tuple],
+        target: Union[torch.Tensor, Dict[str, Any], tuple],
+        model: SequentialDict,
+        **kwargs
+    ):
         pass
 
-    def on_examine_end(self, adversary, input, target, model, **kwargs):
+    def on_examine_end(
+        self,
+        adversary: torch.nn.Module,
+        input: Union[torch.Tensor, tuple],
+        target: Union[torch.Tensor, Dict[str, Any], tuple],
+        model: SequentialDict,
+        **kwargs
+    ):
         pass
 
-    def on_advance_start(self, adversary, input, target, model, **kwargs):
+    def on_advance_start(
+        self,
+        adversary: torch.nn.Module,
+        input: Union[torch.Tensor, tuple],
+        target: Union[torch.Tensor, Dict[str, Any], tuple],
+        model: SequentialDict,
+        **kwargs
+    ):
         pass
 
-    def on_advance_end(self, adversary, input, target, model, **kwargs):
+    def on_advance_end(
+        self,
+        adversary: torch.nn.Module,
+        input: Union[torch.Tensor, tuple],
+        target: Union[torch.Tensor, Dict[str, Any], tuple],
+        model: SequentialDict,
+        **kwargs
+    ):
         pass
 
-    def on_run_end(self, adversary, input, target, model, **kwargs):
+    def on_run_end(
+        self,
+        adversary: torch.nn.Module,
+        input: Union[torch.Tensor, tuple],
+        target: Union[torch.Tensor, Dict[str, Any], tuple],
+        model: SequentialDict,
+        **kwargs
+    ):
         pass
