65a788f766a58f116ae3ccade0934994480b99a3
Weilin Xu <weilin.xu@intel.com>
2023-02-03 14:26:28 -0800

    Revert "Replace strip_ansi with _tty_out=False."
    This reverts commit 589df6bc9b7e4c9c3e15067d75c40f00d62c10b7.

--- tests/helpers/run_sh_command.py
+++ tests/helpers/run_sh_command.py
@@ -2,6 +2,7 @@ from io import StringIO
 from typing import List
 
 import pytest
+from strip_ansi import strip_ansi
 
 from tests.helpers.package_available import _SH_AVAILABLE
 
@@ -14,9 +15,10 @@ def run_sh_command(command: List[str]):
     msg = None
     try:
         with StringIO() as buf:
-            # Turn off color by _tty_out=False
-            sh.python(command, _out=buf, _tty_out=False)
+            sh.python(command, _out=buf)
             output = buf.getvalue()
+            # Remove color.
+            output = strip_ansi(output)
         return output
     except sh.ErrorReturnCode as e:
         msg = e.stderr.decode()
