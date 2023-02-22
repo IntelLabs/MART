9cf66ef846c9e87e63b260ba35db23522f0b4b87
Weilin Xu <weilin.xu@intel.com>
2023-02-21 16:04:29 -0800

    Revert changes in CallableAdapter and make a new PartialInstanceWrapper.

--- mart/utils/adapters.py
+++ mart/utils/adapters.py
@@ -7,49 +7,54 @@
 
 from typing import Any, Callable
 
-__all__ = ["CallableAdapter", "PartialAdapter"]
+__all__ = ["CallableAdapter", "PartialInstanceWrapper"]
 
 
 class CallableAdapter:
     """Adapter to make an object callable."""
 
-    def __init__(self, redirecting_fn):
-        """
-
-        Args:
-            redirecting_fn (str): name of the function that will be invoked in the `__call__` method.
-        """
-        assert redirecting_fn != ""
-
-        self.redirecting_fn = redirecting_fn
-
-    def __call__(self, instance):
+    def __init__(self, instance, redirecting_fn):
         """
 
         Args:
             instance (object): instance to make callable.
+            redirecting_fn (str): name of the function that will be invoked in the `__call__` method.
+        """
+        assert instance is not None
+        assert redirecting_fn != ""
+
+        self.instance = instance
+        self.redirecting_fn = redirecting_fn
+
+    def __call__(self, *args, **kwargs):
         """
 
-        function = getattr(instance, self.redirecting_fn)
+        Args:
+            args (Any): values to use in the callable method.
+            kwargs (Any): keyword values to use in the callable method.
+        """
+        function = getattr(self.instance, self.redirecting_fn)
+
         assert callable(function)
-        return function
+
+        return function(*args, **kwargs)
 
 
-class PartialAdapter:
+class PartialInstanceWrapper:
     """Make a partial class object callable."""
 
-    def __init__(self, partial: Callable, adapter: Callable):
+    def __init__(self, partial: Callable, redirecting_fn: str):
         """
 
         Args:
             partial (Callable): A partial of a class object.
-            adapter (Callable): An adapter to make an object callable.
+            redirecting_fn (str): name of the function that will be invoked in the `__call__` method.
         """
         self.partial = partial
-        self.adapter = adapter
+        self.redirecting_fn = redirecting_fn
 
-    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+    def __call__(self, *args: Any, **kwargs: Any) -> Callable:
         # Turn a partial to a class object.
         instance = self.partial(*args, **kwargs)
         # Make the object callable.
-        return self.adapter(instance)
+        return CallableAdapter(instance, self.redirecting_fn)
