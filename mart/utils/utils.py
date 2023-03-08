c326d10242a90a35c9a4a4f87b2f73cbcda66a7d
Cory Cornelius <cory.cornelius@intel.com>
2023-03-07 16:52:39 -0800

    Use proper model summary statistics

--- mart/utils/utils.py
+++ mart/utils/utils.py
@@ -164,13 +164,11 @@ def log_hyperparameters(object_dict: Dict[str, Any]) -> None:
     hparams["model"] = cfg["model"]
 
     # save number of model parameters
-    hparams["model/params/total"] = sum(p.numel() for p in model.parameters())
-    hparams["model/params/trainable"] = sum(
-        p.numel() for p in model.parameters() if p.requires_grad
-    )
-    hparams["model/params/non_trainable"] = sum(
-        p.numel() for p in model.parameters() if not p.requires_grad
-    )
+    summary = model.summarize()
+
+    hparams["model/params/total"] = summary.total_parameters
+    hparams["model/params/trainable"] = summary.trainable_parameters
+    hparams["model/params/non_trainable"] = summary.total_parameters - summary.trainable_parameters
 
     hparams["datamodule"] = cfg["datamodule"]
     hparams["trainer"] = cfg["trainer"]
