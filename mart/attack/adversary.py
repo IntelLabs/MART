4d95a7b1b27ceb7cbd6e2e07e79da4eb41b14aeb
Cory Cornelius <cory.cornelius@intel.com>
2023-03-10 08:12:40 -0800

    Revert "Integrate perturber into IterativeGenerator"
    This reverts commit e73f9fa3fa0d78602d9db02c48d45f05b2e70320.

--- mart/attack/adversary.py
+++ mart/attack/adversary.py
@@ -7,19 +7,15 @@
 from __future__ import annotations
 
 from collections import OrderedDict
-from typing import TYPE_CHECKING, Any
+from typing import Any
 
 import torch
 
 from .callbacks import Callback
 from .gain import Gain
 from .objective import Objective
+from .perturber import BatchPerturber, Perturber
 from .threat_model import ThreatModel
-
-if TYPE_CHECKING:
-    from ..gradient_modifier import GradientModifier
-    from ..initializer import Initializer
-    from ..projector import Projector
 
 __all__ = ["Adversary", "NoAdversary"]
 
@@ -84,44 +80,38 @@ class IterativeGenerator(AdversaryCallbackHookMixin, torch.nn.Module):
     def __init__(
         self,
         *,
-        optimizer: torch.optim.Optimizer,
-        initializer: Initializer,
+        perturber: BatchPerturber | Perturber,
         max_iters: int,
         gain: Gain,
-        gradient_modifier: GradientModifier | None = None,
-        projector: Projector | None = None,
         objective: Objective | None = None,
         callbacks: dict[str, Callback] | None = None,
     ):
         """_summary_
 
         Args:
-            optimizer (torch.optim.Optimizer): A PyTorch optimizer.
-            initializer (Initializer): To initialize the perturbation.
+            perturber (BatchPerturber | Perturber): A module that stores perturbations.
             max_iters (int): The max number of attack iterations.
             gain (Gain): An adversarial gain function, which is a differentiable estimate of adversarial objective.
-            gradient_modifier (GradientModifier | None): To modify the gradient of perturbation.
-            projector (Projector | None): To project the perturbation into some space.
             objective (Objective | None): A function for computing adversarial objective, which returns True or False. Optional.
             callbacks (dict[str, Callback] | None): A dictionary of callback objects. Optional.
         """
         super().__init__()
 
-        self.optimizer_fn = optimizer
-        self.initializer = initializer
-        self.gradient_modifier = gradient_modifier
-        self.projector = projector
+        self.perturber = perturber
         self.max_iters = max_iters
         self.callbacks = OrderedDict()
+
+        # Register perturber as callback if it implements Callback interface
+        if isinstance(self.perturber, Callback):
+            # FIXME: Use self.perturber.__class__.__name__ as key?
+            self.callbacks["_perturber"] = self.perturber
 
         if callbacks is not None:
             self.callbacks.update(callbacks)
 
         self.objective_fn = objective
+        # self.gain is a tensor.
         self.gain_fn = gain
-
-        # Pre-occupy the name of the buffer, so that extra_repr() always gets perturbation.
-        self.register_buffer("perturbation", torch.nn.UninitializedBuffer(), persistent=False)
 
     @property
     def done(self) -> bool:
@@ -156,7 +146,6 @@ class IterativeGenerator(AdversaryCallbackHookMixin, torch.nn.Module):
         self.cur_iter = 0
 
         self.on_run_start(adversary=self, input=input, target=target, model=model, **kwargs)
-        self.run_start(input)
 
         while True:
             try:
@@ -171,13 +160,25 @@ class IterativeGenerator(AdversaryCallbackHookMixin, torch.nn.Module):
                 # Check the done condition here, so that every update of perturbation is examined.
                 if not self.done:
                     self.on_advance_start(
-                        adversary=self, input=input, target=target, model=model, **kwargs
+                        adversary=self,
+                        input=input,
+                        target=target,
+                        model=model,
+                        **kwargs,
                     )
-                    self.advance(input=input, target=target, model=model, **kwargs)
+                    self.advance(
+                        input=input,
+                        target=target,
+                        model=model,
+                        **kwargs,
+                    )
                     self.on_advance_end(
-                        adversary=self, input=input, target=target, model=model, **kwargs
+                        adversary=self,
+                        input=input,
+                        target=target,
+                        model=model,
+                        **kwargs,
                     )
-
                     # Update cur_iter at the end so that all hooks get the correct cur_iter.
                     self.cur_iter += 1
                 else:
@@ -185,19 +186,7 @@ class IterativeGenerator(AdversaryCallbackHookMixin, torch.nn.Module):
             except StopIteration:
                 break
 
-        self.run_end()
         self.on_run_end(adversary=self, input=input, target=target, model=model, **kwargs)
-
-    def run_start(self, input: torch.Tensor | tuple):
-        # Initialize perturbation.
-        perturbation = self.initializer(input)
-
-        # Register perturbation as a non-persistent buffer even though we will optimize it. This is because it is not
-        # a parameter of the underlying model but a parameter of the adversary.
-        self.register_buffer("perturbation", perturbation, persistent=False)
-
-        # Initialize optimizer
-        self.optimizer = self.optimizer_fn([self.perturbation])
 
     # Make sure we can do autograd.
     # Earlier Pytorch Lightning uses no_grad(), but later PL uses inference_mode():
@@ -252,19 +241,8 @@ class IterativeGenerator(AdversaryCallbackHookMixin, torch.nn.Module):
         **kwargs,
     ):
         """Run one attack iteration."""
-        self.optimizer.zero_grad()
-
         # Do not flip the gain value, because we set maximize=True in optimizer.
         self.total_gain.backward()
-
-        # A backward hook that will be called when a gradient w.r.t the Tensor is computed.
-        with torch.no_grad():
-            self.perturbation.grad = self.gradient_modifier(self.perturbation.grad)
-            self.optimizer.step()
-            self.projector(self.perturbation, input, target)
-
-    def run_end(self):
-        del self.optimizer
 
 
 class Adversary(IterativeGenerator):
@@ -288,13 +266,14 @@ class Adversary(IterativeGenerator):
         **kwargs,
     ):
         # Generate a perturbation only if we have a model. This will update
-        # the parameters of the perturbation.
+        # the parameters of self.perturber.
         if model is not None:
             super().forward(input=input, target=target, model=model, **kwargs)
 
         # Get perturbation and apply threat model
         # The mask projector in perturber may require information from target.
-        output = self.threat_model(input, target, self.perturbation)
+        perturbation = self.perturber(input, target)
+        output = self.threat_model(input, target, perturbation)
 
         return output
 
---
+++ mart/attack/perturber/perturber.py
@@ -0,0 +1,136 @@
+#
+# Copyright (C) 2022 Intel Corporation
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+from __future__ import annotations
+
+import typing
+from typing import Any
+
+import torch
+
+from ..callbacks import Callback
+
+if typing.TYPE_CHECKING:
+    from ..adversary import Adversary
+    from ..gradient_modifier import GradientModifier
+    from ..initializer import Initializer
+    from ..projector import Projector
+
+__all__ = ["Perturber"]
+
+
+class Perturber(Callback, torch.nn.Module):
+    """The base class of perturbers.
+
+    A perturber wraps a nn.Parameter and returns this parameter when called. It also enables one to
+    specify an initialization for this parameter, how to modify gradients computed on this
+    parameter, and how to project the values of the parameter.
+    """
+
+    def __init__(
+        self,
+        optimizer: torch.optim.Optimizer,
+        initializer: Initializer,
+        gradient_modifier: GradientModifier | None = None,
+        projector: Projector | None = None,
+    ):
+        """_summary_
+
+        Args:
+            optimizer (torch.optim.Optimizer): A PyTorch optimizer.
+            initializer (Initializer): To initialize the perturbation.
+            gradient_modifier (GradientModifier | None): To modify the gradient of perturbation.
+            projector (Projector | None): To project the perturbation into some space.
+        """
+        super().__init__()
+
+        self.optimizer_fn = optimizer
+        self.initializer = initializer
+        self.gradient_modifier = gradient_modifier
+        self.projector = projector
+
+        # Pre-occupy the name of the buffer, so that extra_repr() always gets perturbation.
+        self.register_buffer("perturbation", torch.nn.UninitializedBuffer(), persistent=False)
+
+        def projector_wrapper(perturber_module, args):
+            if isinstance(perturber_module.perturbation, torch.nn.UninitializedBuffer):
+                raise ValueError("Perturbation must be initialized")
+
+            input, target = args
+            return projector(perturber_module.perturbation.data, input, target)
+
+        # Will be called before forward() is called.
+        if projector is not None:
+            self.register_forward_pre_hook(projector_wrapper)
+
+    def on_run_start(
+        self,
+        *,
+        adversary: Adversary,
+        input: torch.Tensor | tuple,
+        target: torch.Tensor | dict[str, Any] | tuple,
+        model: torch.nn.Module,
+        **kwargs,
+    ):
+        # Initialize perturbation.
+        perturbation = torch.zeros_like(input, requires_grad=True)
+
+        # Register perturbation as a non-persistent buffer even though we will optimize it. This is because it is not
+        # a parameter of the underlying model but a parameter of the adversary.
+        self.register_buffer("perturbation", perturbation, persistent=False)
+
+        # A backward hook that will be called when a gradient w.r.t the Tensor is computed.
+        if self.gradient_modifier is not None:
+            self.perturbation.register_hook(self.gradient_modifier)
+
+        self.initializer(self.perturbation)
+
+        # Initialize optimizer
+        self.opt = self.optimizer_fn([self.perturbation])
+
+    def on_advance_start(
+        self,
+        *,
+        adversary: Adversary,
+        input: torch.Tensor | tuple,
+        target: torch.Tensor | dict[str, Any] | tuple,
+        model: torch.nn.Module,
+        **kwargs,
+    ):
+        self.opt.zero_grad()
+
+    def on_advance_end(
+        self,
+        *,
+        adversary: Adversary,
+        input: torch.Tensor | tuple,
+        target: torch.Tensor | dict[str, Any] | tuple,
+        model: torch.nn.Module,
+        **kwargs,
+    ):
+        self.opt.step()
+
+    def forward(self, input: torch.Tensor, target: torch.Tensor | dict[str, Any]) -> torch.Tensor:
+        return self.perturbation
+
+    def on_run_end(
+        self,
+        *,
+        adversary: Adversary,
+        input: torch.Tensor | tuple,
+        target: torch.Tensor | dict[str, Any] | tuple,
+        model: torch.nn.Module,
+        **kwargs,
+    ):
+        del self.opt
+
+    def extra_repr(self):
+        perturbation = self.perturbation
+
+        return (
+            f"{repr(perturbation)}, optimizer={self.optimizer_fn}, initializer={self.initializer},"
+            f"gradient_modifier={self.gradient_modifier}, projector={self.projector}"
+        )
