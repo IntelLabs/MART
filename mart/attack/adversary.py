260fd3dd2237f9f0892bd4bcbfa52a6919eb1c0b
Cory Cornelius <cory.cornelius@intel.com>
2023-03-23 14:12:48 -0700

    Late bind trainer to input device

--- mart/attack/adversary.py
+++ mart/attack/adversary.py
@@ -6,6 +6,7 @@
 
 from __future__ import annotations
 
+from functools import partial
 from itertools import cycle
 
 import pytorch_lightning as pl
@@ -39,23 +40,28 @@ class Adversary(torch.nn.Module):
         """
         super().__init__()
 
-        self.attacker = attacker or pl.Trainer(
-            accelerator="auto",  # FIXME: we need to get this on the same device as input...
-            num_sanity_val_steps=0,
-            logger=False,
-            max_epochs=0,
-            limit_train_batches=kwargs.pop("max_iters", 10),
-            callbacks=list(kwargs.pop("callbacks", {}).values()),
-            enable_model_summary=False,
-            enable_checkpointing=False,
-            enable_progress_bar=False,
-        )
+        self.attacker = attacker
 
-        # We feed the same batch to the attack every time so we treat each step as an
-        # attack iteration. As such, attackers must only run for 1 epoch and must limit
-        # the number of attack steps via limit_train_batches.
-        assert self.attacker.max_epochs == 0
-        assert self.attacker.limit_train_batches > 0
+        if self.attacker is None:
+            # Enable attack to be late bound in forward
+            self.attacker = partial(
+                pl.Trainer,
+                num_sanity_val_steps=0,
+                logger=False,
+                max_epochs=0,
+                limit_train_batches=kwargs.pop("max_iters", 10),
+                callbacks=kwargs.pop("callbacks", {}),
+                enable_model_summary=False,
+                enable_checkpointing=False,
+                enable_progress_bar=False,
+            )
+
+        else:
+            # We feed the same batch to the attack every time so we treat each step as an
+            # attack iteration. As such, attackers must only run for 1 epoch and must limit
+            # the number of attack steps via limit_train_batches.
+            assert self.attacker.max_epochs == 0
+            assert self.attacker.limit_train_batches > 0
 
         self.perturber = perturber or Perturber(**kwargs)
         self.enforcer = enforcer
@@ -67,6 +73,24 @@ class Adversary(torch.nn.Module):
         # Adversary lives inside the model, we also need the remaining sequence to be able to
         # get a loss.
         if "model" in batch and "sequence" in batch:
+            # Late bind attacker on same device as input
+            if isinstance(self.attacker, partial):
+                device = batch["input"].device
+
+                if device.type == "cuda":
+                    accelerator = "gpu"
+                    devices = [device.index]
+
+                elif device.type == "cpu":
+                    accelerator = "cpu"
+                    devices = None
+
+                else:
+                    accelerator = device.type
+                    devices = [device.index]
+
+                self.attacker = self.attacker(accelerator=accelerator, devices=devices)
+
             # Attack, aka fit a perturbation, for one epoch by cycling over the same input batch.
             # We use Trainer.limit_train_batches to control the number of attack iterations.
             self.attacker.fit_loop.max_epochs += 1
