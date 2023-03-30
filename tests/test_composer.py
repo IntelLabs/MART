b8e423483a9b237ea7c242fe86c3ba78c9d5b5db
Weilin Xu <weilin.xu@intel.com>
2023-03-30 11:13:45 -0700

    Add tuple and dict test.

--- tests/test_composer.py
+++ tests/test_composer.py
@@ -28,20 +28,44 @@ def test_overlay_composer_forward(input_data, target_data, perturbation):
 
 
 def test_modality_composer_forward(input_data, target_data, perturbation):
-    input = [{"rgb": input_data, "depth": input_data}]
-    target = [target_data]
-    pert = [{"rgb": perturbation, "depth": perturbation}]
+    input = {"rgb": input_data, "depth": input_data}
+    target = target_data
+    pert = {"rgb": perturbation, "depth": perturbation}
 
     rgb_composer = Additive()
     depth_composer = Overlay()
     modality_composer = ModalityComposer(rgb=rgb_composer, depth=depth_composer)
 
+    # dict
     output = modality_composer(pert, input=input, target=target)
+    expected_output = {
+        "rgb": rgb_composer(perturbation, input=input_data, target=target_data),
+        "depth": depth_composer(perturbation, input=input_data, target=target_data),
+    }
+    assert type(output) == type(expected_output)
+    torch.testing.assert_close(output["rgb"], expected_output["rgb"], equal_nan=True)
+    torch.testing.assert_close(output["depth"], expected_output["depth"], equal_nan=True)
+
+    # list of dict
+    output = modality_composer([pert], input=[input], target=[target])
     expected_output = [
         {
             "rgb": rgb_composer(perturbation, input=input_data, target=target_data),
             "depth": depth_composer(perturbation, input=input_data, target=target_data),
         }
     ]
+    assert type(output) == type(expected_output)
+    torch.testing.assert_close(output[0]["rgb"], expected_output[0]["rgb"], equal_nan=True)
+    torch.testing.assert_close(output[0]["depth"], expected_output[0]["depth"], equal_nan=True)
+
+    # tuple of dict
+    output = modality_composer((pert,), input=(input,), target=(target,))
+    expected_output = (
+        {
+            "rgb": rgb_composer(perturbation, input=input_data, target=target_data),
+            "depth": depth_composer(perturbation, input=input_data, target=target_data),
+        },
+    )
+    assert type(output) == type(expected_output)
     torch.testing.assert_close(output[0]["rgb"], expected_output[0]["rgb"], equal_nan=True)
     torch.testing.assert_close(output[0]["depth"], expected_output[0]["depth"], equal_nan=True)
