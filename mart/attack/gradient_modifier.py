4338e08bc6c0c08207873b660f99795598d7e889
Cory Cornelius <cory.cornelius@intel.com>
2023-05-16 08:57:19 -0700

    Replace tuple with Iterable[torch.Tensor] (#134)
    * Replace tuple with Iterable[torch.Tensor]
    * Make GradientModifier accept Iterable[torch.Tensor]
    * Fix annotations

--- mart/attack/gradient_modifier.py
+++ mart/attack/gradient_modifier.py
@@ -6,7 +6,6 @@
 
 from __future__ import annotations
 
-import abc
 from typing import Iterable
 
 import torch
@@ -14,36 +13,33 @@ import torch
 __all__ = ["GradientModifier"]
 
 
-class GradientModifier(abc.ABC):
+class GradientModifier:
     """Gradient modifier base class."""
 
-    def __call__(self, parameters: torch.Tensor | Iterable[torch.Tensor]) -> None:
-        pass
-
-
-class Sign(GradientModifier):
     def __call__(self, parameters: torch.Tensor | Iterable[torch.Tensor]) -> None:
         if isinstance(parameters, torch.Tensor):
             parameters = [parameters]
 
-        parameters = [p for p in parameters if p.grad is not None]
+        [self.modify_(parameter) for parameter in parameters]
 
-        for p in parameters:
-            p.grad.detach().sign_()
+    @torch.no_grad()
+    def modify_(self, parameter: torch.Tensor) -> None:
+        pass
+
+
+class Sign(GradientModifier):
+    @torch.no_grad()
+    def modify_(self, parameter: torch.Tensor) -> None:
+        parameter.grad.sign_()
 
 
 class LpNormalizer(GradientModifier):
     """Scale gradients by a certain L-p norm."""
 
     def __init__(self, p: int | float):
-        self.p = p
+        self.p = float(p)
 
-    def __call__(self, parameters: torch.Tensor | Iterable[torch.Tensor]) -> None:
-        if isinstance(parameters, torch.Tensor):
-            parameters = [parameters]
-
-        parameters = [p for p in parameters if p.grad is not None]
-
-        for p in parameters:
-            p_norm = torch.norm(p.grad.detach(), p=self.p)
-            p.grad.detach().div_(p_norm)
+    @torch.no_grad()
+    def modify_(self, parameter: torch.Tensor) -> None:
+        p_norm = torch.norm(parameter.grad.detach(), p=self.p)
+        parameter.grad.detach().div_(p_norm)
