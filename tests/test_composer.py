c8e9db8745881cc031f777444762cfc05dcc7b2c
Cory Cornelius <cory.cornelius@intel.com>
2023-06-02 13:07:07 -0700

    Remove MaskAdditive test

--- tests/test_composer.py
+++ tests/test_composer.py
@@ -6,7 +6,7 @@
 
 import torch
 
-from mart.attack.composer import Additive, Composite, MaskAdditive
+from mart.attack.composer import Additive, Composite
 
 
 def test_additive_composer_forward(input_data, target_data, perturbation):
@@ -24,15 +24,4 @@ def test_overlay_composer_forward(input_data, target_data, perturbation):
     mask = target_data["perturbable_mask"]
     mask = mask.to(input_data)
     expected_output = input_data * (1 - mask) + perturbation
-    torch.testing.assert_close(output, expected_output, equal_nan=True)
-
-
-def test_mask_additive_composer_forward():
-    input = torch.zeros((2, 2))
-    perturbation = torch.ones((2, 2))
-    target = {"perturbable_mask": torch.eye(2)}
-    expected_output = torch.eye(2)
-
-    composer = MaskAdditive()
-    output = composer(perturbation, input=input, target=target)
     torch.testing.assert_close(output, expected_output, equal_nan=True)
