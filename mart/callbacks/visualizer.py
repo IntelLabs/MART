353c281c0f906d6354d811a2c7f3d14802d847c3
Cory Cornelius <cory.cornelius@intel.com>
2023-06-09 22:40:47 -0700

    ImageVisualizer consumes outputs

--- mart/callbacks/visualizer.py
+++ mart/callbacks/visualizer.py
@@ -12,9 +12,9 @@ __all__ = ["ImageVisualizer"]
 
 
 class ImageVisualizer(Callback):
-    def __init__(self, frequency: int = 100, **tag_paths):
+    def __init__(self, frequency: int = 100, **tag_keys):
         self.frequency = frequency
-        self.tag_paths = tag_paths
+        self.tag_keys = tag_keys
 
     def log_image(self, trainer, tag, image):
         # Add image to each logger
@@ -23,18 +23,17 @@ class ImageVisualizer(Callback):
             if not hasattr(logger.experiment, "add_image"):
                 continue
 
-            logger.experiment.add_image(tag, image, global_step=trainer.global_step)
-
-    def log_images(self, trainer, pl_module):
-        for tag, path in self.tag_paths.items():
-            image = attrgetter(path)(pl_module)
-            self.log_image(trainer, tag, image)
+            if len(image.shape) == 4:
+                logger.experiment.add_images(tag, image, global_step=trainer.global_step)
+            elif len(image.shape) == 3:
+                logger.experiment.add_image(tag, image, global_step=trainer.global_step)
+            else:
+                raise ValueError(f"Unsupported image shape: {image.shape}")
 
     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
         if batch_idx % self.frequency != 0:
             return
 
-        self.log_images(trainer, pl_module)
-
-    def on_train_end(self, trainer, pl_module):
-        self.log_images(trainer, pl_module)
+        for tag, output_key in self.tag_keys.items():
+            image = outputs[output_key]
+            self.log_image(trainer, tag, image)
