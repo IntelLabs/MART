04a249aa37c63def98549dff42d2332b65d0760f
Cory Cornelius <cory.cornelius@intel.com>
2023-06-14 14:39:14 -0700

    Better Magic Module

--- mart/nn/module.py
+++ mart/nn/module.py
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
+import types
+from collections import OrderedDict
+
 import torch
 from hydra.utils import instantiate
 
@@ -11,15 +14,21 @@ __all__ = ["Module"]
 
 
 class Module(torch.nn.Module):
-    def __init__(self, _path_: str, *args, **kwargs):
-        super().__init__()
+    """A magic Module that can override forward."""
 
-        # TODO: Add _load_state_dict_
-        # TODO: Add _freeze_
+    def __new__(cls, *args, _path_: str, **kwargs):
+        # TODO: Add support for _load_state_dict_
+        # TODO: Add support for _freeze_
 
         cfg = {"_target_": _path_}
-        self.module = instantiate(cfg, *args, **kwargs)
+        module = instantiate(cfg, *args, **kwargs)
 
+        module._forward = module.forward
+        module.forward = types.MethodType(Module.forward, module)
+
+        return module
+
+    @staticmethod
     def forward(
         self,
         *args,
@@ -27,12 +36,12 @@ class Module(torch.nn.Module):
         inference_mode: bool = False,
         **kwargs,
     ):
-        old_train_mode = self.module.training
+        old_train_mode = self.training
 
         # FIXME: Would be nice if this was a context...
-        self.module.train(train_mode)
+        self.train(train_mode)
         with torch.inference_mode(mode=inference_mode):
-            ret = self.module(*args, **kwargs)
-        self.module.train(old_train_mode)
+            ret = self._forward(*args, **kwargs)
+        self.train(old_train_mode)
 
         return ret
