3737c9876be1c3416b72d414f804a127736f318c
Weilin Xu <weilin.xu@intel.com>
2023-06-20 14:28:45 -0700

    Comment.

--- mart/utils/modality_dispatch.py
+++ mart/utils/modality_dispatch.py
@@ -32,59 +32,7 @@ def modality_dispatch(
     The function returns an object that is homomorphic to input.
     """
 
-    raise ValueError(
-        f"Unsupported data type combination: type(input)={type(input)} and type(data)={type(data)}."
-    )
-
-    # if (
-    #     isinstance(input, torch.Tensor)
-    #     and isinstance(data, torch.Tensor)  # noqa: W503
-    #     and isinstance(modality_func, dict)  # noqa: W503
-    # ):
-    #     # A dictionary of Callable indexed by modality.
-    #     return modality_func[modality](data, input=input, target=target)
-    # elif (
-    #     isinstance(input, torch.Tensor)
-    #     and isinstance(data, torch.Tensor)  # noqa: W503
-    #     and isinstance(modality_func, Callable)  # noqa: W503
-    # ):
-    #     # A Callable with modality=? as a keyword argument.
-    #     return modality_func(data, input=input, target=target, modality=modality)
-    # elif isinstance(input, dict):
-    #     # The dict input has modalities specified in keys, passing them recursively.
-    #     output = {}
-    #     for modality in input.keys():
-    #         output[modality] = modality_dispatch(
-    #             modality_func,
-    #             data[modality],
-    #             input=input[modality],
-    #             target=target,
-    #             modality=modality,
-    #         )
-    #     return output
-    # elif isinstance(input, (list, tuple)):
-    #     # The list or tuple input is a collection of sub-input and sub-target.
-    #     if not isinstance(target, (list, tuple)):
-    #         # Make target zip well with input.
-    #         target = cycle([target])
-    #     if not isinstance(data, (list, tuple)):
-    #         # Data is shared for all input, e.g. universal perturbation.
-    #         # Make data zip well with input.
-    #         data = cycle([data])
-
-    #     output = []
-    #     for data_i, input_i, target_i in zip(data, input, target):
-    #         output_i = modality_dispatch(
-    #             modality_func, data_i, input=input_i, target=target_i, modality=modality
-    #         )
-    #         output.append(output_i)
-    #     if isinstance(input, tuple):
-    #         output = tuple(output)
-    #     return output
-    # else:
-    #     raise ValueError(
-    #         f"Unsupported data type combination: type(input)={type(input)} and type(data)={type(data)}."
-    #     )
+    raise ValueError(f"Unsupported data type of input: type(input)={type(input)}.")
 
 
 @modality_dispatch.register
