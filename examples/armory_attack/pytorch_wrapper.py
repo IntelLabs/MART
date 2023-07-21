570a8b5f32285df54683cb3cc0d12034a2df2711
Weilin Xu <weilin.xu@intel.com>
2023-07-21 10:21:56 -0700

    Implement wrapper MartAttackObjectDetection.

--- examples/armory_attack/pytorch_wrapper.py
+++ examples/armory_attack/pytorch_wrapper.py
@@ -12,6 +12,8 @@ import torch
 from multimethod import multimethod
 from omegaconf import OmegaConf
 
+from .batch_converter import ObjectDetectionBatchConverter
+
 
 # A recursive function to convert all np.ndarray in an object to torch.Tensor, or vice versa.
 @multimethod
@@ -55,25 +57,59 @@ class MartAttack:
     4. Convert torch.Tensor back to np.ndarray.
     """
 
-    def __init__(self, model, mart_adv_config_yaml):
+    def __init__(self, model, batch_converter, mart_adv_config_yaml):
         # TODO: Automatically search for torch.nn.Module within model.
         # Extract PyTorch model from an ART Estimator.
         self.model = model._model
         self.device = self.model.device
 
+        self.batch_converter = batch_converter
+
         # Instantiate a MART adversary.
         adv_cfg = OmegaConf.load(mart_adv_config_yaml)
         self.adversary = hydra.utils.instantiate(adv_cfg)
 
-    def generate(self, **batch_np):
-        # Convert np.ndarray to torch.Tensor.
-        # Specify a device to place PyTorch tensors.
-        batch_pth = convert(batch_np, device=self.device)
-        batch_adv_pytorch = self.adversary(batch_pth, model=self.model)
+    def convert_batch_armory_to_torchvision(self, batch_armory_np):
+        # np.ndarray -> torch.Tensor, on a device.
+        batch_armory_pth = convert(batch_armory_np, device=self.device)
+        # armory format -> torchvision format.
+        batch_tv_pth = self.batch_converter(batch_armory_pth)
+        return batch_tv_pth
 
-        # Convert torch.Tensor to np.ndarray.
-        batch_adv_np = convert(batch_adv_pytorch)
+    def convert_batch_torchvision_to_armory(self, batch_tv_pth):
+        # torchvision format -> armory format.
+        batch_armory_pth = self.batch_converter.revert(batch_tv_pth)
+        # torch.Tensor -> np.ndarray
+        batch_armory_np = convert(batch_armory_pth)
+        return batch_armory_np
+
+    def generate(self, **batch_armory_np):
+        batch_tv_pth = self.convert_batch_armory_to_torchvision(batch_armory_np)
+        batch_adv_tv_pth = self.adversary(batch_tv_pth, model=self.model)
+        batch_adv_armory_np = self.convert_batch_torchvision_to_armory(batch_adv_tv_pth)
 
         # Only return adversarial input in the original numpy format.
-        input_adv_np = batch_adv_np["x"]
+        input_key = self.batch_converter.input_key
+        input_adv_np = batch_adv_armory_np[input_key]
         return input_adv_np
+
+
+class MartAttackObjectDetection(MartAttack):
+    def __init__(self, model, mart_adv_config_yaml):
+        batch_config = {
+            "input_key": "x",
+            "target_keys": {
+                "y": ["area", "boxes", "id", "image_id", "is_crowd", "labels"],
+                "y_patch_metadata": [
+                    "avg_patch_depth",
+                    "gs_coords",
+                    "mask",
+                    "max_depth_perturb_meters",
+                ],
+            },
+        }
+
+        batch_converter = ObjectDetectionBatchConverter(**batch_config)
+        super().__init__(
+            model=model, batch_converter=batch_converter, mart_adv_config_yaml=mart_adv_config_yaml
+        )
