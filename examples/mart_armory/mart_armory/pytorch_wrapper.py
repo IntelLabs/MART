f187adbae93e89ec15fd1986b47b492c1d613d03
Weilin Xu <weilin.xu@intel.com>
2023-07-26 17:35:49 -0700

    Replace the multimethod package with functools.singledispatch.

--- examples/mart_armory/mart_armory/pytorch_wrapper.py
+++ examples/mart_armory/mart_armory/pytorch_wrapper.py
@@ -6,45 +6,46 @@
 
 from __future__ import annotations
 
+from functools import singledispatch
+
 import hydra
 import numpy as np
 import torch
-from multimethod import multimethod
 from omegaconf import OmegaConf
 
 from mart.models.dual_mode import DualModeGeneralizedRCNN
 
 
 # A recursive function to convert all np.ndarray in an object to torch.Tensor, or vice versa.
-@multimethod
-def convert(obj: dict, device=None):
+@singledispatch
+def convert(obj, device=None):
+    """All other types, no change."""
+    return obj
+
+
+@convert.register
+def _(obj: dict, device=None):
     return {key: convert(value, device=device) for key, value in obj.items()}
 
 
-@multimethod
-def convert(obj: list, device=None):  # noqa: F811
+@convert.register
+def _(obj: list, device=None):
     return [convert(item, device=device) for item in obj]
 
 
-@multimethod
-def convert(obj: tuple, device=None):  # noqa: F811
+@convert.register
+def _(obj: tuple, device=None):
     return tuple(convert(obj, device=device))
 
 
-@multimethod
-def convert(obj: np.ndarray, device=None):  # noqa: F811
+@convert.register
+def _(obj: np.ndarray, device=None):
     return torch.tensor(obj, device=device)
 
 
-@multimethod
-def convert(obj: torch.Tensor, device=None):  # noqa: F811
+@convert.register
+def _(obj: torch.Tensor, device=None):
     return obj.detach().cpu().numpy()
-
-
-# All other types, no change.
-@multimethod
-def convert(obj, device=None):  # noqa: F811
-    return obj
 
 
 class ArtRcnnModelWrapper(torch.nn.Module):
