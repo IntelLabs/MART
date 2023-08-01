2b91fe2c217761f2485e051a669640c99f77f79a
Weilin Xu <weilin.xu@intel.com>
2023-07-31 17:10:43 -0700

    Fix import path.

--- examples/mart_armory/mart_armory/__init__.py
+++ examples/mart_armory/mart_armory/__init__.py
@@ -1,5 +1,5 @@
-import importlib
+from importlib import metadata
 
 from mart_armory.pytorch_wrapper import MartAttack
 
-__version__ = importlib.metadata.version(__package__ or __name__)
+__version__ = metadata.version(__package__ or __name__)
