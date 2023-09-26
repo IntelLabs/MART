11c7a47d67d043a1782c7d08031b6dae55727b42
Weilin Xu <weilin.xu@intel.com>
2023-09-25 21:08:10 -0700

    Add composer functions for rectangle patch.

--- mart/attack/composer.py
+++ mart/attack/composer.py
@@ -11,6 +11,7 @@ from collections import OrderedDict
 from typing import Any, Iterable
 
 import torch
+from torchvision.transforms import functional as F
 
 
 class Function(torch.nn.Module):
@@ -114,4 +115,115 @@ class Overlay(Function):
         perturbation = perturbation * mask
 
         input = input * (1 - mask) + perturbation
+        return perturbation, input, target
+
+
+class RectangleCrop(Function):
+    def __init__(self, *args, coords_key="patch_coords", **kwargs):
+        super().__init__(*args, **kwargs)
+        self.coords_key = coords_key
+
+    def get_smallest_rectangle_shape(self, input, patch_coords):
+        """Get a smallest rectangle that covers the whole patch."""
+        coords = patch_coords
+        leading_dims = list(input.shape[:-2])
+        width = coords[:, 0].max() - coords[:, 0].min()
+        height = coords[:, 1].max() - coords[:, 1].min()
+        shape = list(leading_dims) + [height, width]
+        return shape
+
+    def slice_rectangle(self, perturbation, height_patch, width_patch):
+        """Slice a rectangle from top-left of the perturbation."""
+        height_patch_index = torch.tensor(range(height_patch), device=perturbation.device)
+        width_patch_index = torch.tensor(range(width_patch), device=perturbation.device)
+        perturbation_patch = perturbation.index_select(-2, height_patch_index).index_select(
+            -1, width_patch_index
+        )
+        return perturbation_patch
+
+    def forward(self, perturbation, input, target):
+        coords = target[self.coords_key]
+        # TODO: Make composers stackable to reuse some Composer.
+        # The perturbation variable has the same shape as input.
+        #    We slice a small rectangle from top-left of the perturbation variable to compose the patch.
+        rectangle_shape = self.get_smallest_rectangle_shape(input, coords)
+        # Assume perturbation is in shape of [N]CHW
+        height_patch, width_patch = rectangle_shape[-2:]
+        rectangle_patch = self.slice_rectangle(perturbation, height_patch, width_patch)
+        return rectangle_patch, input, target
+
+
+class RectanglePad(Function):
+    def __init__(self, *args, coords_key="patch_coords", rect_coords_key="rect_coords", **kwargs):
+        super().__init__(*args, **kwargs)
+        self.coords_key = coords_key
+        self.rect_coords_key = rect_coords_key
+
+    def forward(self, perturbation_patch, input, target):
+        coords = target[self.coords_key]
+        height, width = input.shape[-2:]
+        # Pad rectangle to the same size of input, so that it is almost aligned with the patch.
+        height_patch, width_patch = perturbation_patch.shape[-2:]
+        pad_left = min(coords[0, 0], coords[3, 0])
+        pad_top = min(coords[0, 1], coords[1, 1])
+        pad_right = width - width_patch - pad_left
+        pad_bottom = height - height_patch - pad_top
+
+        perturbation_padded = F.pad(
+            img=perturbation_patch,
+            padding=[pad_left, pad_top, pad_right, pad_bottom],
+            fill=0,
+            padding_mode="constant",
+        )
+
+        # Save coords of four corners of the rectangle for later transform.
+        top_left = [pad_left, pad_top]
+        top_right = [width - pad_right, pad_top]
+        bottom_right = [width - pad_right, height - pad_bottom]
+        bottom_left = [pad_left, height - pad_bottom]
+        target[self.rect_coords_key] = [top_left, top_right, bottom_right, bottom_left]
+
+        return perturbation_padded, input, target
+
+
+class RectanglePerspectiveTransform(Function):
+    def __init__(self, *args, coords_key="patch_coords", rect_coords_key="rect_coords", **kwargs):
+        super().__init__(*args, **kwargs)
+        self.coords_key = coords_key
+        self.rect_coords_key = rect_coords_key
+
+    def forward(self, perturbation_rect, input, target):
+        coords = target[self.coords_key]
+        # Perspective transformation: rectangle -> coords.
+        # Fetch four corners of the rectangle.
+        startpoints = target[self.rect_coords_key]
+        endpoints = coords
+        # TODO: Make interpolation configurable.
+        perturbation_coords = F.perspective(
+            img=perturbation_rect,
+            startpoints=startpoints,
+            endpoints=endpoints,
+            interpolation=F.InterpolationMode.BILINEAR,
+            fill=0,
+        )
+        return perturbation_coords, input, target
+
+
+class FakeClamp(Function):
+    """A Clamp operation that preserves gradients."""
+
+    def __init__(self, *args, min_val, max_val, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.min_val = min_val
+        self.max_val = max_val
+
+    @staticmethod
+    def fake_clamp(x, *, min_val, max_val):
+        with torch.no_grad():
+            x_clamped = x.clamp(min_val, max_val)
+            diff = x_clamped - x
+        return x + diff
+
+    def forward(self, perturbation, input, target):
+        input = self.fake_clamp(input, min_val=self.min_val, max_val=self.max_val)
         return perturbation, input, target
--- tests/test_composer.py
+++ tests/test_composer.py
@@ -6,7 +6,15 @@
 
 import torch
 
-from mart.attack.composer import Additive, Composer, Mask, Overlay
+from mart.attack.composer import (
+    Additive,
+    Composer,
+    Mask,
+    Overlay,
+    RectangleCrop,
+    RectanglePad,
+    RectanglePerspectiveTransform,
+)
 
 
 def test_additive_composer_forward(input_data, target_data, perturbation):
@@ -36,3 +44,73 @@ def test_mask_additive_composer_forward():
     composer = Composer(functions={"mask": Mask(order=0), "additive": Additive(order=1)})
     output = composer(perturbation, input=input, target=target)
     torch.testing.assert_close(output, expected_output, equal_nan=True)
+
+
+def test_rect_crop():
+    key = "patch_coords"
+    input = torch.zeros((3, 10, 10))
+    perturbation = torch.ones_like(input)
+    fn = RectangleCrop(coords_key=key)
+
+    # FIXME: four corner points (width, height) of a patch in the order of top-left, top-right, bottom-right, bottom-left.
+    # A simple square patch.
+    patch_coords = torch.tensor(((0, 0), (5, 0), (5, 5), (5, 0)))
+    target = {key: patch_coords}
+
+    rect_patch, _input, _target = fn(perturbation, input, target)
+    assert torch.equal(input, _input)
+    assert target == _target
+    assert rect_patch.shape == (3, 5, 5)
+
+    # A skew patch.
+    patch_coords = torch.tensor(((1, 1), (5, 2), (7, 8), (3, 9)))
+    target = {key: patch_coords}
+
+    rect_patch, _input, _target = fn(perturbation, input, target)
+    assert torch.equal(input, _input)
+    assert target == _target
+    assert rect_patch.shape == (3, 8, 6)
+
+
+def test_rect_pad():
+    coords_key = "patch_coords"
+    rect_coords_key = "rect_coords"
+
+    rect_patch = torch.ones(3, 5, 5)
+    patch_coords = torch.tensor(((0, 0), (5, 0), (5, 5), (5, 0)))
+
+    input = torch.zeros((3, 10, 10))
+    target = {coords_key: patch_coords}
+
+    fn = RectanglePad(coords_key=coords_key, rect_coords_key=rect_coords_key)
+    pert_padded, _input, _target = fn(rect_patch, input, target)
+
+    pert_padded_expected = torch.zeros_like(input)
+    pert_padded_expected[:, :5, :5] = 1
+
+    assert torch.equal(pert_padded_expected, pert_padded)
+
+    rect_coords_expected = [[0, 0], [5, 0], [5, 5], [0, 5]]
+    assert _target[rect_coords_key] == rect_coords_expected
+
+
+def test_rect_perspective_transform():
+    coords_key = "patch_coords"
+    rect_coords_key = "rect_coords"
+
+    rect_coords = [[0, 0], [5, 0], [5, 5], [0, 5]]
+    # Move from top left to bottom right.
+    patch_coords = torch.tensor(((5, 5), (10, 5), (10, 10), (5, 10)))
+    target = {coords_key: patch_coords, rect_coords_key: rect_coords}
+
+    input = torch.zeros((3, 10, 10))
+
+    pert_padded = torch.zeros_like(input)
+    pert_padded[:, :5, :5] = 1
+
+    fn = RectanglePerspectiveTransform(coords_key=coords_key, rect_coords_key=rect_coords_key)
+    pert_coords, _input, _target = fn(pert_padded, input, target)
+    pert_coords_expected = torch.zeros_like(input)
+    pert_coords_expected[:, 5:, 5:] = 1
+    # rounding numeric error from the perspective transformation.
+    assert torch.equal(pert_coords.round(), pert_coords_expected)
