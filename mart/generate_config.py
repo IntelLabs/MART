a8f484c9d64d9a72658a510d911d3358fff73ca5
Weilin Xu <weilin.xu@intel.com>
2023-09-27 06:22:45 -0700

    Modualize components.

--- mart/generate_config.py
+++ mart/generate_config.py
@@ -11,7 +11,38 @@ from hydra import compose, initialize_config_dir
 from omegaconf import OmegaConf
 
 
-def generate(
+def mart_compose(
+    *overrides,
+    version_base: str = "1.2",
+    config_dir: str = "configs",
+    config_name: str = "lightning.yaml",
+    export_node: str = None,
+):
+    # Add an absolute path {config_dir} to the search path of configs, preceding those in mart.configs.
+    if not os.path.isabs(config_dir):
+        config_dir = os.path.abspath(config_dir)
+
+    # initialize_config_dir() requires an absolute path.
+    with initialize_config_dir(version_base=version_base, config_dir=config_dir):
+        cfg = compose(config_name=config_name, overrides=overrides)
+
+    # Export a sub-tree.
+    if export_node is not None:
+        for key in export_node.split("."):
+            cfg = cfg[key]
+
+    return cfg
+
+
+def get_yaml_cfg(cfg, resolve: bool = False):
+    # Resolve all interpolation in the sub-tree.
+    if resolve:
+        OmegaConf.resolve(cfg)
+
+    return OmegaConf.to_yaml(cfg)
+
+
+def main(
     *overrides,
     version_base: str = "1.2",
     config_dir: str = "configs",
@@ -19,25 +50,19 @@ def generate(
     export_node: str = None,
     resolve: bool = False,
 ):
-    # An absolute path {config_dir} is added to the search path of configs, preceding those in mart.configs.
-    if not os.path.isabs(config_dir):
-        config_dir = os.path.abspath(config_dir)
+    cfg = mart_compose(
+        *overrides,
+        version_base=version_base,
+        config_dir=config_dir,
+        config_name=config_name,
+        export_node=export_node,
+    )
 
-    with initialize_config_dir(version_base=version_base, config_dir=config_dir):
-        cfg = compose(config_name=config_name, overrides=overrides)
+    cfg_yaml = get_yaml_cfg(cfg, resolve=resolve)
 
-        # Export a sub-tree.
-        if export_node is not None:
-            for key in export_node.split("."):
-                cfg = cfg[key]
-
-        # Resolve all interpolation in the sub-tree.
-        if resolve:
-            OmegaConf.resolve(cfg)
-
-        # OmegaConf.to_yaml() already ends with `\n`.
-        print(OmegaConf.to_yaml(cfg), end="")
+    # OmegaConf.to_yaml() already ends with `\n`.
+    print(cfg_yaml, end="")
 
 
 if __name__ == "__main__":
-    fire.Fire(generate)
+    fire.Fire(main)
