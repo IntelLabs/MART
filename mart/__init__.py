a18b691303d783e4eb5f1c28bc155a154dcd18be
Weilin Xu <weilin.xu@intel.com>
2024-05-02 13:18:01 -0700

    Avoid importing everything in mart.

--- mart/__init__.py
+++ mart/__init__.py
@@ -1,11 +1,3 @@
-import importlib
-
-from mart import attack as attack
-from mart import datamodules as datamodules
-from mart import models as models
-from mart import nn as nn
-from mart import optim as optim
-from mart import transforms as transforms
-from mart import utils as utils
+import importlib.metadata
 
 __version__ = importlib.metadata.version(__package__ or __name__)
