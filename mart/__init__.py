c2fb57e7fd05804f328f661c18f4c88cea8b8d7d
Weilin Xu <weilin.xu@intel.com>
2024-05-08 12:55:31 -0700

    Revert "Avoid importing unused submodules."
    This reverts commit c70ee9690fe8d9626d2e0b22c43ca0e59802dc45.

--- mart/__init__.py
+++ mart/__init__.py
@@ -1,3 +1,11 @@
-import importlib.metadata
+import importlib
+
+from mart import attack as attack
+from mart import datamodules as datamodules
+from mart import models as models
+from mart import nn as nn
+from mart import optim as optim
+from mart import transforms as transforms
+from mart import utils as utils
 
 __version__ = importlib.metadata.version(__package__ or __name__)
