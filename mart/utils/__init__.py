3111b51d4f337d5f8f56acd6f385079fe962c3ec
Weilin Xu <weilin.xu@intel.com>
2024-05-09 10:40:21 -0700

    Skip importing vision-related utils if torchvision is not installed..

--- mart/utils/__init__.py
+++ mart/utils/__init__.py
@@ -1,8 +1,11 @@
 from .adapters import *
 from .config import *
-from .export import *
+from .imports import _HAS_TORCHVISION
 from .monkey_patch import *
 from .pylogger import *
 from .rich_utils import *
 from .silent import *
 from .utils import *
+
+if _HAS_TORCHVISION:
+    from .export import *
