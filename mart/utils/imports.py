6c6331ee5dc8cff939a0c4a210c763ab9175d0d0
Weilin Xu <weilin.xu@intel.com>
2024-05-10 09:58:30 -0700

    Add a one-time warning message about uninstalled packages.

--- mart/utils/imports.py
+++ mart/utils/imports.py
@@ -6,9 +6,18 @@
 
 from importlib.util import find_spec
 
+from .pylogger import get_pylogger
+
+logger = get_pylogger(__name__)
+
 
 def has(module_name):
-    return find_spec(module_name) is not None
+    module = find_spec(module_name)
+    if module is None:
+        logger.warn(f"{module_name} is not installed, so some features in MART are unavailable.")
+        return False
+    else:
+        return True
 
 
 _HAS_FIFTYONE = has("fiftyone")
