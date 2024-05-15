8d19d9bbd472707cdaead80924e623d03613ac90
Weilin Xu <weilin.xu@intel.com>
2024-05-15 12:13:19 -0700

    Make a package anomalib_adversary

---
+++ examples/anomalib_adversary/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="anomalib_adversary",
+    version="0.1.0a",
+    description="Evaluating robustness of anomaly detection models in Anomalib.",
+    packages=["anomalib_adversary"],
+)
