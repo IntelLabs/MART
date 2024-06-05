e0117ae8a07d242267e53cb443ff3abcf5b48c69
Weilin Xu <weilin.xu@intel.com>
2024-06-05 10:40:41 -0700

    Mordenize packaging with pyproject.toml.

--- examples/anomalib_adversary/setup.py
+++
@@ -1,8 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="anomalib_adversary",
-    version="0.1.0a",
-    description="Evaluating robustness of anomaly detection models in Anomalib.",
-    packages=["anomalib_adversary"],
-)
