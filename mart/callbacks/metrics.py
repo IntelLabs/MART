4e86f6fe303f2076bce997c73c71649338a48ca1
Weilin Xu <weilin.xu@intel.com>
2025-05-07 11:25:44 -0700

    Split the callback into two.

--- mart/callbacks/metrics.py
+++ mart/callbacks/metrics.py
@@ -17,16 +17,15 @@ from ..nn.nn import DotDict
 
 logger = logging.getLogger(__name__)
 
+__all__ = ["Metrics"]
 
-class LogMetrics(Callback):
+
+class Metrics(Callback):
     """For models returning a dictionary, we can configure the callback to log scalars from the
     outputs, calculate and log metrics."""
 
     def __init__(
         self,
-        train_step_log: Sequence | dict = None,
-        val_step_log: Sequence | dict = None,
-        test_step_log: Sequence | dict = None,
         train_metrics: Metric = None,
         val_metrics: Metric = None,
         test_metrics: Metric = None,
@@ -39,26 +38,6 @@ class LogMetrics(Callback):
     ):
         super().__init__()
 
-        # Be backwards compatible by turning list into dict where each item is its own key-value
-        if isinstance(train_step_log, (list, tuple)):
-            train_step_log = {item: {"key": item, "prog_bar": True} for item in train_step_log}
-        train_step_log = train_step_log or {}
-
-        # Be backwards compatible by turning list into dict where each item is its own key-value
-        if isinstance(val_step_log, (list, tuple)):
-            val_step_log = {item: {"key": item, "prog_bar": True} for item in val_step_log}
-        val_step_log = val_step_log or {}
-
-        # Be backwards compatible by turning list into dict where each item is its own key-value
-        if isinstance(test_step_log, (list, tuple)):
-            test_step_log = {item: {"key": item, "prog_bar": True} for item in test_step_log}
-        test_step_log = test_step_log or {}
-
-        self.step_log = {
-            "train": train_step_log,
-            "val": val_step_log,
-            "test": test_step_log,
-        }
         self.metrics = {
             "train": train_metrics,
             "val": val_metrics,
@@ -97,11 +76,6 @@ class LogMetrics(Callback):
     def on_batch_end(self, outputs, *, prefix: str):
         # Convert to DotDict, so that we can use a dot-connected string as a key to find a value deep in the dictionary.
         outputs = DotDict(outputs)
-
-        step_log = self.step_log[prefix]
-        for log_name, cfg in step_log.items():
-            key, prog_bar = cfg["key"], cfg["prog_bar"]
-            self.log(f"{prefix}/{log_name}", outputs[key], prog_bar=prog_bar)
 
         metric = self.metrics[prefix]
         if metric is not None:
