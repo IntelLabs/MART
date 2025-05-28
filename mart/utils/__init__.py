9af8ba2b47617892b92bfeb392f866cac727ca6f
Weilin Xu <weilin.xu@intel.com>
2025-05-27 17:41:06 -0700

    Conditional imports in mart.utils.

--- mart/utils/__init__.py
+++ mart/utils/__init__.py
@@ -1,11 +1,14 @@
 from .adapters import *
-from .config import *
-from .imports import _HAS_TORCHVISION
+from .imports import _HAS_LIGHTNING, _HAS_TORCHVISION
 from .monkey_patch import *
 from .pylogger import *
-from .rich_utils import *
 from .silent import *
-from .utils import *
+
+if _HAS_LIGHTNING:
+    from .config import *
+    from .rich_utils import *
+    from .utils import *
+
 
 if _HAS_TORCHVISION:
     from .export import *
