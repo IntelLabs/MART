f27a9d53d2659465dc6314819fb7f6abeb031fc7
Weilin Xu <weilin.xu@intel.com>
2025-06-02 16:37:13 -0700

    Revert "Fallback to the basic python logger if lightning is not installed."
    This reverts commit e0bae506ace06547c71f6d45affb7850b9068084.

--- mart/utils/pylogger.py
+++ mart/utils/pylogger.py
@@ -1,9 +1,6 @@
 import logging
 
-from .imports import _HAS_LIGHTNING
-
-if _HAS_LIGHTNING:
-    from lightning.pytorch.utilities import rank_zero_only
+from lightning.pytorch.utilities import rank_zero_only
 
 __all__ = ["get_pylogger"]
 
@@ -15,10 +12,8 @@ def get_pylogger(name=__name__) -> logging.Logger:
 
     # this ensures all logging levels get marked with the rank zero decorator
     # otherwise logs would get multiplied for each GPU process in multi-GPU setup
-    if _HAS_LIGHTNING:
-        logging_levels = ("debug", "info", "warning", "error", "exception", "fatal", "critical")
-        for level in logging_levels:
-            setattr(logger, level, rank_zero_only(getattr(logger, level)))
-    # Otherwise, fallback to the regular logger if lightning is not installed.
+    logging_levels = ("debug", "info", "warning", "error", "exception", "fatal", "critical")
+    for level in logging_levels:
+        setattr(logger, level, rank_zero_only(getattr(logger, level)))
 
     return logger
