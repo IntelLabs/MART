33cd7065d1da1578a45b0109425fbde79ccb9010
Weilin Xu <weilin.xu@intel.com>
2025-06-02 16:47:11 -0700

    Conditional imports for lightnign utils.

--- mart/utils/__init__.py
+++ mart/utils/__init__.py
@@ -1,5 +1,11 @@
 # Only import components without external dependency.
 from .adapters import *
+from .imports import _HAS_LIGHTNING
 from .monkey_patch import *
 from .optimization import *
 from .silent import *
+
+if _HAS_LIGHTNING:
+    from .lightning import *
+    from .pylogger import *
+    from .rich_utils import *
